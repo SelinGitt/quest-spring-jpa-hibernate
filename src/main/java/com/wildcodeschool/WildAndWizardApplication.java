6711e076d688c7014b23accb6de3e58e76f6a175
agzibuyukselin@gmail.com <agzibuyukselin@gmail.com>
2023-06-23 11:12:53 +0200

    update all class for the quest to be complete

--- src/main/java/com/wildcodeschool/WildAndWizardApplication.java
+++ src/main/java/com/wildcodeschool/WildAndWizardApplication.java
@@ -1,4 +1,4 @@
-package com.wildcodeschool.wildandwizard;
+package com.wildcodeschool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
